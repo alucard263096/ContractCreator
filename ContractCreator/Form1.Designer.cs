808250bbe081dbdef006953fac2fa269396ff80a
Steve Cai <steve.cai@morningstar.com>
2016-05-03 17:23:35 +0800

    功能完成

---
+++ ContractCreator/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ContractCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDownDemo = new System.Windows.Forms.Button();
+            this.pgb = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "使用说明（必读）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(906, 143);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(868, 259);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "生成";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDownDemo
+            // 
+            this.btnDownDemo.Location = new System.Drawing.Point(787, 259);
+            this.btnDownDemo.Name = "btnDownDemo";
+            this.btnDownDemo.Size = new System.Drawing.Size(75, 23);
+            this.btnDownDemo.TabIndex = 3;
+            this.btnDownDemo.Text = "下载模板";
+            this.btnDownDemo.UseVisualStyleBackColor = true;
+            this.btnDownDemo.Click += new System.EventHandler(this.btnDownDemo_Click);
+            // 
+            // pgb
+            // 
+            this.pgb.Location = new System.Drawing.Point(12, 202);
+            this.pgb.Name = "pgb";
+            this.pgb.Size = new System.Drawing.Size(931, 23);
+            this.pgb.TabIndex = 4;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Location = new System.Drawing.Point(762, 228);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblProgress.Size = new System.Drawing.Size(184, 23);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.Text = "100/100";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 232);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 44);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "深圳海富特资讯管理公司版权所有";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 290);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pgb);
+            this.Controls.Add(this.btnDownDemo);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "合同生成器";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDownDemo;
+        private System.Windows.Forms.ProgressBar pgb;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ContractCreator/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContractCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog docFileDialog = new OpenFileDialog();
+            docFileDialog.Title = "选择一个合同模板文件";
+            docFileDialog.Filter = "Word文件 (*.docx)|*.docx|2003Word文件 (*.doc)|*.doc";
+            docFileDialog.FilterIndex = 1;
+            if (docFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OpenFileDialog excelFileDialog = new OpenFileDialog();
+                excelFileDialog.Title = "选择一个合同数据文件";
+                excelFileDialog.Filter = "Excel文件 (*.xlsx)|*.xlsx|2003Excel文件 (*.xls)|*.xls";
+                excelFileDialog.FilterIndex = 1;
+                if (excelFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FolderBrowserDialog savePathDialog = new FolderBrowserDialog();
+                    savePathDialog.Description = "选择合同生成的存放路径";
+                    if (savePathDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        RDoC r=new RDoC(docFileDialog.FileName, excelFileDialog.FileName, savePathDialog.SelectedPath,this);
+                        Thread t = new Thread(new ThreadStart(r.rdo));
+                        t.Start();  
+                        //StartGenerateContract(docFileDialog.FileName, excelFileDialog.FileName, savePathDialog.SelectedPath);
+                    }
+                }
+            }
+        }
+
+        class RDoC
+        {
+            string doc, exc, svp;
+            Form1 f;
+            public RDoC(string doc, string exc, string svp,Form1 f)
+            {
+                this.doc = doc;
+                this.exc = exc;
+                this.svp = svp;
+                this.f = f;
+            }
+            public void rdo()
+            {
+                StartGenerateContract(doc, exc, svp);
+            }
+            private void StartGenerateContract(string docFilePath, string excelFilePath, string savePath)
+            {
+                bool canOpenWord = TryToOpenWord(docFilePath);
+                if (canOpenWord == false)
+                {
+                    MessageBox.Show("读取合同模板文档失败，请检查Word文件或者Office环境是否正确。");
+                    return;
+                }
+                DataTable dtExcel = ExcelToDT(excelFilePath);
+                if (dtExcel == null)
+                {
+                    MessageBox.Show("读取合同数据文档失败，请检查Excel文件或者Office环境是否正确。");
+                    return;
+                }
+                f.btnCreate.Enabled = false;
+                f.pgb.Maximum = dtExcel.Rows.Count;
+                f.pgb.Value = 0;
+                for (int i = 0; i < dtExcel.Rows.Count; i++)
+                {
+                    GenerateContract(docFilePath, dtExcel.Columns, dtExcel.Rows[i], savePath, getNo(i + 1, dtExcel.Rows.Count));
+                    f.pgb.Value++;
+                    f.lblProgress.Text = (i + 1).ToString() + "/" + dtExcel.Rows.Count.ToString();
+                }
+                f.lblProgress.Text = "生成完成";
+                f.btnCreate.Enabled = true;
+            }
+
+            private string getNo(int no, int total)
+            {
+                StringBuilder sb = new StringBuilder();
+                string nostr = no.ToString();
+                string totalstr = total.ToString();
+                for (int i = 0; i < totalstr.Length - nostr.Length; i++)
+                {
+                    sb.Append("0");
+                }
+                sb.Append(no);
+                return sb.ToString();
+            }
+
+            private void GenerateContract(string docFilePath, DataColumnCollection columns, DataRow row, string savePath, string no)
+            {
+                Microsoft.Office.Interop.Word.Application app = null;
+                Microsoft.Office.Interop.Word.Document doc = null;
+                //将要导出的新word文件名
+                try
+                {
+                    app = new Microsoft.Office.Interop.Word.Application();//创建word应用程序
+                    object fileName = docFilePath;//模板文件
+                    //打开模板文件
+                    object oMissing = System.Reflection.Missing.Value;
+                    doc = app.Documents.Open(ref fileName,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+                    object replace = Microsoft.Office.Interop.Word.WdReplace.wdReplaceAll;
+                    //构造数据
+
+                    foreach (DataColumn col in columns)
+                    {
+                        app.Selection.Find.Replacement.ClearFormatting();
+                        app.Selection.Find.ClearFormatting();
+                        app.Selection.Find.Text = "{" + col.ColumnName + "}";//需要被替换的文本
+                        app.Selection.Find.Replacement.Text = row[col.ColumnName].ToString();//替换文本 
+
+                        //执行替换操作
+                        app.Selection.Find.Execute(
+                        ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing, ref oMissing,
+                        ref oMissing, ref replace,
+                        ref oMissing, ref oMissing,
+                        ref oMissing, ref oMissing);
+                    }
+
+                    //对替换好的word模板另存为一个新的word文档
+                    object newfileName = savePath + "\\" + no.ToString() + row[0].ToString() + ".docx";//模板文件
+
+                    doc.SaveAs(newfileName,
+                    oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing, oMissing,
+                    oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+                }
+                catch (System.Threading.ThreadAbortException ex)
+                {
+                    //这边为了捕获Response.End引起的异常
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    if (doc != null)
+                    {
+                        doc.Close();//关闭word文档
+                    }
+                    if (app != null)
+                    {
+                        app.Quit();//退出word应用程序
+                    }
+                }
+            }
+
+            private bool TryToOpenWord(string docFilePath)
+            {
+                Microsoft.Office.Interop.Word.Application app = null;
+                Microsoft.Office.Interop.Word.Document doc = null;
+                //将要导出的新word文件名
+                try
+                {
+                    app = new Microsoft.Office.Interop.Word.Application();//创建word应用程序
+                    object fileName = docFilePath;//模板文件
+                    //打开模板文件
+                    object oMissing = System.Reflection.Missing.Value;
+                    doc = app.Documents.Open(ref fileName,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                    return true;
+                }
+                catch (System.Threading.ThreadAbortException ex)
+                {
+                    //这边为了捕获Response.End引起的异常
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    if (doc != null)
+                    {
+                        doc.Close();//关闭word文档
+                    }
+                    if (app != null)
+                    {
+                        app.Quit();//退出word应用程序
+                    }
+                }
+                return false;
+            }
+
+            public DataTable ExcelToDT(string Path)
+            {
+                try
+                {
+                    string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + Path + ";" + @"Extended Properties=""Excel 12.0 Xml;HDR=YES;""";
+                    using (OleDbConnection conn = new OleDbConnection(strConn))
+                    {
+                        conn.Open();
+                        string strExcel = "";
+                        OleDbDataAdapter myCommand = null;
+                        DataSet ds = null;
+                        strExcel = "select * from [sheet1$]";
+                        myCommand = new OleDbDataAdapter(strExcel, strConn);
+                        ds = new DataSet();
+                        myCommand.Fill(ds, "table1");
+                        return ds.Tables[0];
+                    }
+                }
+                catch
+                {
+
+                }
+                return null;
+            }
+        }
+
+        private void btnDownDemo_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog savePathDialog = new FolderBrowserDialog();
+            savePathDialog.Description = "选择模板合同的存放路径";
+            if (savePathDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo doc=new FileInfo("Templates\\合同模板.docx");
+                doc.CopyTo(savePathDialog.SelectedPath + "\\" + doc.Name,true);
+
+                FileInfo excel = new FileInfo("Templates\\合同数据.xlsx");
+                excel.CopyTo(savePathDialog.SelectedPath + "\\" + excel.Name, true);
+
+
+                string path = savePathDialog.SelectedPath;
+                System.Diagnostics.Process.Start("explorer.exe", path);
+            }
+        }
+    }
+}
