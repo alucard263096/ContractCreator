d8f56e646dda7943b2b4931d04f90a3195d83f31
Steve Cai <steve.cai@morningstar.com>
2016-05-03 17:28:00 +0800

    添加公司注释

--- ContractCreator/Properties/AssemblyInfo.cs
+++ ContractCreator/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ContractCreator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("这是一个自动批量生成合同文件的应用程序，通过读取Excel的值生成到Word中")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("海富特资讯管理有限公司")]
 [assembly: AssemblyProduct("ContractCreator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Helpfooter")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
